d371b96ba33ea871d871678b25bcde9bd3bbb1b0
ModermHTMLDesign <modernhtmldesign@outlook.com>
2020-06-01 07:32:53 -0400

    Added 2 new pages.
    Going to update the download buttons soon.

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var hide = document.getElementById("output");
+var hide = document.getElementById("dldopt");
+var hideDownloadWindows = document.getElementById("windows");
+var hideDownloadUnixBase = document.getElementById("unix");
 function goReg() {
     var c1 = document.getElementById('txt_py').value;
     var d1 = document.getElementById('textArea');
 	var hide = document.getElementById("dldopt");
 	var show = document.getElementById("showopt");
-    d1.innerHTML = '<p>' + c1 + '</p>';
+	d1.innerHTML = '<p>' + c1 + '</p>';
+	String(d1)
     hide.style.display = "inline-block";
 	show.style.display = "inline-block";
 }
 function goLarge() {
     var c1 = document.getElementById('txt_py').value;
-    var d1 = document.getElementById('textArea');
+	var d1 = document.getElementById('textArea');
+	var hide = document.getElementById("dldopt")
 	var show = document.getElementById("showopt");
-    d1.innerHTML = '<h2>' + c1 + '</h2>';
+	d1.innerHTML = '<h2>' + c1 + '</h2>';
+	String(d1)
 	hide.style.display = "inline-block";
 	show.style.display = "inline-block";
 }
@@ -20,7 +25,8 @@ function goFancy() {
     var c1 = document.getElementById('txt_py').value;
     var d1 = document.getElementById('textArea');
 	var hide = document.getElementById("dldopt");
-    d1.innerHTML = '<p style="font-family: cursive, sans-serif">' + c1 + '</p>';
+	d1.innerHTML = '<p style="font-family: cursive, sans-serif">' + c1 + '</p>';
+	String(d1)
 	hide.style.display = "none";
 	hide.style.backgroundColor = "blue";
 	hide.style.color = "blue";
@@ -39,7 +45,8 @@ function goLargeFancy() {
     var c1 = document.getElementById('txt_py').value;
     var d1 = document.getElementById('textArea');
 	var hide = document.getElementById("dldopt");
-    d1.innerHTML = '<h2 style="font-family: cursive, sans-serif">' + c1 + '</h2>';
+	d1.innerHTML = '<h2 style="font-family: cursive, sans-serif">' + c1 + '</h2>';
+	String(d1)
 	hide.style.display = "none";
 	hide.style.backgroundColor = "blue";
 	hide.style.color = "blue";
@@ -54,30 +61,37 @@ function goLargeFancy() {
     hide.style.display = "block";
   }
 }
-function saveTextAsFile(textToWrite, fileNameToSaveAs)
-    {
-    	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
-    	var downloadLink = document.createElement("a");
-    	downloadLink.download = fileNameToSaveAs;
-    	downloadLink.innerHTML = "Download File";
-    	if (window.webkitURL != null)
-    	{
-    		// Chrome allows the link to be clicked
-    		// without actually adding it to the DOM.
-    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-    	}
-    	else
-    	{
-    		// Firefox requires the link to be added to the DOM
-    		// before it can be clicked.
-    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    		downloadLink.onclick = destroyClickedElement;
-    		downloadLink.style.display = "none";
-    		document.body.appendChild(downloadLink);
-    	}
-    
-    	downloadLink.click();
-    }
+function saveTextAsFile() {
+		var textToWrite = document.getElementById('textArea').value;
+		var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+		var fileNameToSaveAs = "convert.html";
+	  
+		var downloadLink = document.createElement("a");
+		downloadLink.download = fileNameToSaveAs;
+		downloadLink.innerHTML = "Download File";
+		if (window.webkitURL != null) {
+		  // Chrome allows the link to be clicked without actually adding it to the DOM.
+		  downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+		} else {
+		  // Firefox requires the link to be added to the DOM before it can be clicked.
+		  downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+		  downloadLink.onclick = destroyClickedElement;
+		  downloadLink.style.display = "none";
+		  document.body.appendChild(downloadLink);
+		}
+	  
+		downloadLink.click();
+	  }
+	  
+	  var button = document.getElementById('save');
+	  button.addEventListener('click', saveTextAsFile);
+	  
+	  function destroyClickedElement(event) {
+		// remove the link from the DOM
+		document.body.removeChild(event.target);
+	  }
+
+
 function hideItem() {
   var hide = document.getElementById("output");
   if (hide.style.display === "block") {
@@ -86,33 +100,37 @@ function hideItem() {
     hide.style.display = "block";
   }
 }
-function downloadApp() {
+function downloadCorrectApp() {
+//window.location.href = "sorry.html";
+//window.location.replace("sorry.html");
 	var userAgent = window.navigator.userAgent,
 		platform = window.navigator.platform,
 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
 		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-		scriptdownload = null;
+		os = null;
+	var hideDownloadUnixBase = document.getElementById("unix");
+	var hideDownloadWindows = document.getElementById("windows");
+  
 	if (macosPlatforms.indexOf(platform) !== -1) {
-	  scriptdownload = 'Download for macOS';
-	  document.getElementById("windows").hidden = true;
-	  alert("This has not been tested. Please contect modernhtmldesign@outlook.com if you experience any problems.");
+	  os = 'Mac OS';
+	  hideDownloadWindows.style.display = "none";
+	  hideDownloadUnixBase.style.display = "inline-block";
 	} else if (iosPlatforms.indexOf(platform) !== -1) {
-	  scriptdownload = 'No iOS app yet!';
+	  os = 'iOS';
+	  window.location.href = "sorry.html";
+	  window.location.replace("sorry.html");
 	} else if (windowsPlatforms.indexOf(platform) !== -1) {
-	  scriptdownload = 'Download for Windows (Vista, 7, 8.x, 10)';
-	  document.getElementById("unix").hidden = true;
+	  os = 'Windows';
+	  hideDownloadUnixBase.style.display = "none";
+	  hideDownloadWindows.style.display = "inline-block";
 	} else if (/Android/.test(userAgent)) {
-	  scriptdownload = 'No Android app yet!';
+	  os = 'Android';
+	  window.location.href = "sorry.html";
+	  window.location.replace("sorry.html");
 	} else if (!os && /Linux/.test(platform)) {
-	  scriptdownload = 'Download for Linux';
-	  document.getElementById("windows").hidden = true;
-	  alert("This has been tested in Ubuntu. Please contect modernhtmldesign@outlook.com if you experience any problems.");
+	  os = 'Linux';
 	}
-	return scriptdownload;
-  }
-  var returnDownload = downloadApp();
-  function appType() {
-    document.getElementById("downloadText").innerHTML = scriptdownload;
-    }
-    
+  
+	return os;
+}
